814d3d8c1df952b24ee00e0f323b6b3aa746a665
mindychxn <mindychxn@gmail.com>
2023-09-13 18:53:58 -0400

    Add fonts and animation to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'scale(1)'},
+          '50%': { transform: 'scale(1.05)'}
+        }
+      },
+      animation: {
+        float: 'float 5s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }
