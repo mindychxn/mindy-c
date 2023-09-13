43a342203b867139a81a5d135f89c323510d9ae5
mindychxn <mindychxn@gmail.com>
2023-09-13 19:40:10 -0400

    Add NavBar component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import NavBar from './components/NavBar';
 import LandingPage from './components/LandingPage';
 import Introduction from './components/Introduction';
+import { useState } from 'react';
+
 export default function App() {
   return (
     <div className='bg-black'>
+      <NavBar />
       <LandingPage />
       <Introduction />
     </div>
