26ce96d5855a0c79ca28b3e27660734f6b6e9dd4
mindychxn <mindychxn@gmail.com>
2023-09-14 14:51:03 -0400

    Add landing page text and animations

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,17 +1,27 @@
 import './landingpage.css';
-import profilePic from '../assets/JavaScript frameworks-amico.svg'
+import profilePic from '../assets/landingPage.svg';
 export default function LandingPage() {
   return (
-    <div className='landing-page flex items-center justify-center gap-4 relative h-screen'>
-      <div className='flex flex-col'>
-        <div className='name-text text-7xl font-bold z-10 bg-clip-text bg-gradient-to-tr from-[#f1cefc] via-white to-[#fed4c7] text-transparent p-4'>
-          Hi there, I'm Mindy.
+    <div className='landing-page flex items-center justify-center gap-14 relative h-screen'>
+      <div className='flex flex-col w-[40%] p-10'>
+        <div
+          className='font-semibold z-10 text-[#f8f6e5] text-5xl pl-[17px]'
+        >
+          Hi there,
         </div>
-        {/* <div className='text-white font-montserrat font-light z-10'>
-          I'm a software Developer 
-        </div> */}
+        <div className='name-text font-bold text-8xl z-10 bg-clip-text bg-gradient-to-tr from-[#ffbdfc] via-[#f8f6e5] to-[#f9c8b9] text-transparent p-4'>
+          I'm Mindy.
+        </div>
+        <div className='text-cream tracking-[0.0125em] font-DMSans text-xl font-light z-10 pl-[17px] mt-5'>
+          As a software developer, I aim to create impactful and innovative
+          solutions.
+        </div>
       </div>
-      <img className='w-1/3 animate-float' src={profilePic} alt='' />
+      <img
+        className='w-1/3 animate-float drop-shadow-[1px_5px_10px_rgba(255,252,241,0.5)] drop'
+        src={profilePic}
+        alt=''
+      />
     </div>
   );
 }
