b24c60de6e036a4390a7600a07638a2be2663b81
mindychxn <mindychxn@gmail.com>
2023-09-15 09:10:10 -0400

    Add gradient background

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import computerAnimation from '../assets/computer.json';
 import waveAnimation from '../assets/wave.json';
+import gradient from '../assets/aboutMeGradient.svg';
 import lottie from 'lottie-web';
 
 export default function AboutMe() {
@@ -29,13 +30,21 @@ export default function AboutMe() {
   const waveContainer = useRef(null);
 
   return (
-    <div className='relative flex about-me h-screen w-full font-DMSans justify-center items-center'>
+    <div
+      className='relative flex h-screen w-full font-DMSans justify-center items-center'
+      style={{
+        backgroundImage: `url('${gradient}')`,
+        backgroundSize: 'cover',
+      }}
+    >
       <div className='backdrop-blur-[2px] z-50 bg-[#b05bff19] flex justify-center items-center flex-col h-fit rounded-lg font-DMSans text-white w-1/2'>
         <div
           className='w-[50%] h-[50%] drop-shadow-[1px_5px_10px_rgba(174,47,209,0.83)]'
           ref={computerContainer}
         />
-        <div className='font-semibold text-3xl font-DMSans'>It's nice to meet you</div>
+        <div className='font-semibold text-3xl font-DMSans'>
+          It's nice to meet you
+        </div>
       </div>
       <div
         className='absolute z-0 right-0 bottom-0 w-full drop-shadow-[1px_1px_2px_rgba(242,124,97,1)]'
