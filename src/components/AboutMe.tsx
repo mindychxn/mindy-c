1248f574a86c500ae38395a43bbbd98d6f829c7c
mindychxn <mindychxn@gmail.com>
2023-09-15 01:07:07 -0400

    Add lottie animations for AboutMe

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,46 @@
-import computerAnimation from '../../static/computer.json';
+import { useEffect, useRef } from 'react';
+import computerAnimation from '../assets/computer.json';
+import waveAnimation from '../assets/wave.json';
+import lottie from 'lottie-web';
 
 export default function AboutMe() {
+  useEffect(() => {
+    const computer = lottie.loadAnimation({
+      animationData: computerAnimation,
+      autoplay: true,
+      container: computerContainer.current!,
+      loop: true,
+      renderer: 'svg',
+    });
+    const wave = lottie.loadAnimation({
+      animationData: waveAnimation,
+      autoplay: true,
+      container: waveContainer.current!,
+      loop: true,
+      renderer: 'svg',
+    });
+    return () => {
+      computer.destroy();
+      wave.destroy();
+    };
+  }, []);
+
+  const computerContainer = useRef(null);
+  const waveContainer = useRef(null);
+
   return (
-    <div className="h-14 bg-black">
-      <div>It's nice to meet you!</div>
+    <div className='relative flex about-me h-screen w-full font-DMSans justify-center items-center'>
+      <div className='backdrop-blur-[2px] z-50 bg-[#b05bff19] flex justify-center items-center flex-col h-fit rounded-lg font-DMSans text-white w-1/2'>
+        <div
+          className='w-[50%] h-[50%] drop-shadow-[1px_5px_10px_rgba(174,47,209,0.83)]'
+          ref={computerContainer}
+        />
+        <div className='font-semibold text-3xl font-DMSans'>It's nice to meet you</div>
+      </div>
+      <div
+        className='absolute z-0 right-0 bottom-0 w-full drop-shadow-[1px_1px_2px_rgba(242,124,97,1)]'
+        ref={waveContainer}
+      />
     </div>
   );
 }
