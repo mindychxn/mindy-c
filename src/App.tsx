e86c993a55ad45e794b590bec46878926e0b7479
mindychxn <mindychxn@gmail.com>
2023-09-16 16:47:21 -0400

    Add logo, add prop to handle mobile NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import AboutMe from './components/AboutMe';
 export default function App() {
   return (
     <div className='bg-black min-w-min'>
-      <NavBar sections={['Home', 'Projects', 'About', 'Contact', 'Resume']} />
+      <NavBar sections={['Home', 'Projects', 'About', 'Contact', 'Resume']} collapsed={false} className='hidden lg:flex' />
       <LandingPage />
       <Projects />
       <AboutMe />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,36 +1,36 @@
 import React, { useState } from 'react';
+import logo from '../assets/logo.svg';
 
-export default function NavBar() {
+export interface NavBarProps {
+  sections: string[];
+  collapsed: boolean;
+  className: string;
+}
+export default function NavBar({ sections, collapsed, className = '' }: NavBarProps) {
   const [currentPlace, setCurrentPlace] = useState('Home');
-  const currentStyles = 'bg-white/20 backdrop-blur-md';
-  const commonStyles =
-    'py-3 px-5 rounded-full hover:bg-[#fffcf1dc]/50 transition ease-in-out';
+  const currentStyles = 'shadow-[inset_0_0_20px_#ffffff7a]';
   return (
-    <div className='font-montserrat p-4 z-50 flex justify-end text-white gap-5 w-full fixed rounded-lg'>
-      <button
-        className={`${commonStyles} ${
-          currentPlace === 'Home' ? currentStyles : ''
-        }`}
-        onClick={() => setCurrentPlace('Home')}
-      >
-        Home
-      </button>
-      <button
-        className={`${commonStyles} ${
-          currentPlace === 'Projects' ? currentStyles : ''
-        }`}
-        onClick={() => setCurrentPlace('Projects')}
-      >
-        Projects
-      </button>
-      <button
-        className={`${commonStyles} ${
-          currentPlace === 'About' ? currentStyles : ''
-        }`}
-        onClick={() => setCurrentPlace('About')}
-      >
-        About Me
-      </button>
+    <div className={`font-DMSans leading-widest text-cream px-4 py-5 z-[100] justify-between gap-5 w-full fixed rounded-lg font-light ${className}`}>
+      <div className='xl:w-[15%] sm:w-[25%] w-3/4 h-fit flex items-center rounded-full backdrop-blur-sm py-2.5'>
+        <img
+          className='invert drop-shadow-[1px_5px_5px_#fff6edde] px-5'
+          src={logo}
+          alt=''
+        />
+      </div>
+      <div className='space-x-4 pr-4'>
+        {sections.map((section, i) => (
+          <button
+            key={i}
+            className={`2xl:text-lg py-2.5 px-5 hover:scale-105 hover:translate-y-[0.3rem] rounded-full backdrop-blur-sm duration-500 hover:drop-shadow-[1px_5px_10px_#ffffffa8] transition ease-in-out ${
+              currentPlace === section ? currentStyles : ''
+            }`}
+            onClick={() => setCurrentPlace(section)}
+          >
+            {section}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
