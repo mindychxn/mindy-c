a5554b01fd00f6d00a2102c7c7dd1b87958ac61b
mindychxn <mindychxn@gmail.com>
2023-09-16 13:22:57 -0400

    Add project card styling, play animation on hover

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useRef, useState } from 'react';
 import './projects.css';
 import ProjectCard from './ProjectCard';
 import expenseAnimation from '../assets/expense.json';
-import waveAnimation from '../assets/wave.json';
+import gameAnimation from '../assets/game.json';
 import lottie from 'lottie-web';
 
 export default function Projects() {
   const [playExpenseAnimation, setPlayExpenseAnimation] = useState(false);
+  const [playGameAnimation, setPlayGameAnimation] = useState(false);
+
   useEffect(() => {
     const expense = lottie.loadAnimation({
       animationData: expenseAnimation,
@@ -15,48 +17,55 @@ export default function Projects() {
       loop: true,
       renderer: 'svg',
     });
-    const wave = lottie.loadAnimation({
-      animationData: waveAnimation,
-      autoplay: true,
-      container: waveContainer.current!,
+    const game = lottie.loadAnimation({
+      animationData: gameAnimation,
+      autoplay: playGameAnimation,
+      container: gameContainer.current!,
       loop: true,
       renderer: 'svg',
     });
     return () => {
       expense.destroy();
-      wave.destroy();
+      game.destroy();
     };
-  }, [playExpenseAnimation]);
+  }, [playExpenseAnimation, playGameAnimation]);
   const expenseContainer = useRef(null);
-  const waveContainer = useRef(null);
+  const gameContainer = useRef(null);
   return (
-    <div className='relative flex flex-col gap-10 justify-center items-center w-full projects h-screen'>
-      <div className='font-bold text-6xl text-cream animate-glow bg-clip-text bg-gradient-to-tr from-[#ffb7f8] via-cream to-[#b594fb] text-transparent p-4'>
+    <div className='relative flex flex-col gap-4 sm:gap-7 justify-center items-center w-full projects my-20'>
+      <div className='font-bold text-[44px] sm:text-6xl bg-clip-text bg-gradient-to-tr from-[#9c7eff] via-[#f1e8ff] to-[#fb94f2] text-transparent p-1'>
         Projects
       </div>
-      <div className='grid grid-cols-2 w-2/3 gap-10'>
+      <div className='font-DMSans sm:text-xl font-light text-white w-3/4 text-center'>
+        The best part of coding is undoubtedly being able to design and create
+        your own projects to bring your ideas and concepts to life. Here are
+        some that I have completed so far!
+      </div>
+      <div className='flex-col flex lg:grid grid-cols-2 w-[72%] gap-12 sm:gap-20 mt-6'>
         <ProjectCard
           name='Expense Tracker'
-          description='Something to track your expenses'
-          className='mb-[100px] animate-float1 group/expense'
+          description='To help you track and visualize your expenses'
+          className='lg:mb-[100px] animate-float1'
           onMouseOver={() => setPlayExpenseAnimation(true)}
-          onMouseOut={() => (setPlayExpenseAnimation(false), console.log(playExpenseAnimation))}
+          onMouseOut={() => setPlayExpenseAnimation(false)}
+          right={false}
         >
-          <div className='flex rounded-lg group-hover/expense:bg-gradient-to-r from-cyan-500 to-blue-500'>
-            <div className='z-50' ref={expenseContainer} />
+          <div className='p-6 sm:p-10 rounded-lg bg-gradient-to-b from-[#512ad4] to-indigo-400 shadow-lg'>
+            <div ref={expenseContainer} />
           </div>
         </ProjectCard>
         <ProjectCard
-          name='Expense Tracker'
-          description='Something to track your expenses'
-          onMouseOver={() => setPlayExpenseAnimation(true)}
-          onMouseOut={() => setPlayExpenseAnimation(false)}
-          className='mt-[100px] animate-float2'
-        />
-        <div
-        className='absolute z-0 right-0 -bottom-[100px] w-full drop-shadow-[1px_1px_2px_rgba(242,124,97,1)]'
-        ref={waveContainer}
-      />
+          name='Simon Game'
+          description='The memory game you know and love'
+          onMouseOver={() => setPlayGameAnimation(true)}
+          onMouseOut={() => setPlayGameAnimation(false)}
+          right
+          className='lg:mt-[100px] animate-float2 text-left lg:text-right'
+        >
+          <div className='p-6 sm:p-10 rounded-lg bg-gradient-to-b to-red-400 from-orange-300'>
+            <div ref={gameContainer} />
+          </div>
+        </ProjectCard>
       </div>
     </div>
   );
