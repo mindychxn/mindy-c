7d5a997a0573ac1bdcb9a7b636518ed85bed4753
mindychxn <mindychxn@gmail.com>
2023-09-16 13:21:56 -0400

    Add glow effect on cards, add breakpoint styling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './projectcard.css';
 
 export interface ProjectCardProps {
   children?: React.ReactNode;
@@ -6,6 +7,7 @@ export interface ProjectCardProps {
   description: string;
   onMouseOver(): void;
   onMouseOut(): void;
+  right: boolean;
   className?: string;
 }
 
@@ -15,19 +17,21 @@ export default function ProjectCard({
   description,
   onMouseOver,
   onMouseOut,
+  right,
   className = '',
 }: ProjectCardProps) {
   return (
     <div
-      className={`flex flex-col gap-4 w-full backdrop-blur-md border-cream/5 border text-white bg-[#ffffff10] rounded-lg p-4 transition-all ${className} hover:shadow-[0px_0px_20px_#f8f6e5b3] hover:animate-glow hover:scale-105 hover:transition-all`}
+      className={`project-card flex flex-col w-full backdrop-blur-md border-cream/5 border hover:border-none  text-white bg-[#ffffff10] hover:bg-[#c4d6fc] rounded-lg p-4 transition-all ${className} hover:cursor-pointer hover:transition ease-in-out z-[100] px-8 py-8 h-fit`}
       onMouseOver={onMouseOver}
       onMouseOut={onMouseOut}
     >
-      {children}
-      <div className='flex flex-col'>
-        <div className='text-3xl font-semibold'>{name}</div>
-        <div className='font-DMSans font-light'>{description}</div>
+      {!right && children}
+      <div className={`flex flex-col gap-1 ${right ? 'mb-4' : 'mt-4'}`}>
+        <div className='text-2xl sm:text-4xl font-semibold'>{name}</div>
+        <div className='font-DMSans font-light sm:text-xl'>{description}</div>
       </div>
+      {right && children}
     </div>
   );
 }
